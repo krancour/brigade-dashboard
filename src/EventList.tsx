932c68442ff95e2100401257818c2a5418133f42
Kent Rancourt <kent.rancourt@gmail.com>
2021-12-29 13:42:33 -0500

    add infinite scrolling for projects and events
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- src/EventList.tsx
+++ src/EventList.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { core, meta } from "@brigadecore/brigade-sdk"
+import { core } from "@brigadecore/brigade-sdk"
+import InfiniteScroll from "react-infinite-scroll-component"
 
 import getClient from "./Client"
 
@@ -9,7 +10,15 @@ interface EventListItemProps {
 
 class EventListItem extends React.Component<EventListItemProps> {
   render(): React.ReactElement {
-    return <li>{this.props.event.metadata?.id}</li>
+    // TODO: I don't love embedding this style here, but we NEED this and it
+    // works well enough for now.
+    const style = {
+      height: 30,
+      border: "1px solid green",
+      margin: 6,
+      padding: 8
+    }
+    return <div style={style}>{this.props.event.metadata?.id}</div>
   }
 }
 
@@ -19,6 +28,7 @@ interface EventListProps {
 
 interface EventListState {
   events: core.Event[]
+  continueVal: string
 }
 
 export default class EventList extends React.Component<EventListProps, EventListState> {
@@ -26,13 +36,34 @@ export default class EventList extends React.Component<EventListProps, EventList
   constructor(props: EventListProps) {
     super(props)
     this.state = {
-      events: []
-    }
+      events: [],
+      continueVal: ""
+    } 
   }
 
   async componentDidMount(): Promise<void> {
+    if (this.props.loggedIn) {
+      const events = await getClient().core().events().list({}, {
+        continue: "",
+        limit: 100
+      })
+      this.setState({
+        events: events.items,
+        continueVal: events.metadata.continue || ""
+      })
+    }
+  }
+
+  fetch = async () => {
+    const events = this.state.events
+    const continueVal = this.state.continueVal
+    const newEvents = await getClient().core().events().list({}, {
+      continue: continueVal,
+      limit: 20
+    })
     this.setState({
-      events: (await getClient().core().events().list()).items
+      events: events.concat(newEvents.items),
+      continueVal: newEvents.metadata.continue || ""
     })
   }
 
@@ -41,10 +72,16 @@ export default class EventList extends React.Component<EventListProps, EventList
       return <p>Log in to view events.</p>
     }
     const events = this.state.events
-    const eventListItems = events.map((event: core.Event) => {
-      return <EventListItem event={event}/>
-    })
-    return <ul>{eventListItems}</ul>
+    const hasMore = this.state.continueVal != ""
+    return (
+      <div>
+        <InfiniteScroll dataLength={this.state.events.length} next={this.fetch} hasMore={hasMore} loader={<h4>Loading...</h4>}>
+          {events.map((event: core.Event) => (
+            <EventListItem key={event.metadata?.id} event={event}/>
+          ))}
+        </InfiniteScroll>
+      </div>
+    )
   }
 
 }
--- src/ProjectList.tsx
+++ src/ProjectList.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { core, meta } from "@brigadecore/brigade-sdk"
+import { core } from "@brigadecore/brigade-sdk"
+import InfiniteScroll from "react-infinite-scroll-component"
 
 import getClient from "./Client"
 
@@ -9,7 +10,15 @@ interface ProjectListItemProps {
 
 class ProjectListItem extends React.Component<ProjectListItemProps> {
   render(): React.ReactElement {
-    return <li>{this.props.project.metadata.id}</li>
+    // TODO: I don't love embedding this style here, but we NEED this and it
+    // works well enough for now.
+    const style = {
+      height: 30,
+      border: "1px solid green",
+      margin: 6,
+      padding: 8
+    }
+    return <div style={style}>{this.props.project.metadata.id}</div>
   }
 }
 
@@ -19,6 +28,7 @@ interface ProjectListProps {
 
 interface ProjectListState {
   projects: core.Project[]
+  continueVal: string
 }
 
 export default class ProjectList extends React.Component<ProjectListProps, ProjectListState> {
@@ -26,13 +36,34 @@ export default class ProjectList extends React.Component<ProjectListProps, Proje
   constructor(props: ProjectListProps) {
     super(props)
     this.state = {
-      projects: []
+      projects: [],
+      continueVal: ""
     }
   }
 
   async componentDidMount(): Promise<void> {
+    if (this.props.loggedIn) {
+      const projects = await getClient().core().projects().list({}, {
+        continue: "",
+        limit: 100
+      })
+      this.setState({
+        projects: projects.items,
+        continueVal: projects.metadata.continue || ""
+      })
+    }
+  }
+
+  fetch = async () => {
+    const projects = this.state.projects
+    const continueVal = this.state.continueVal
+    const newProjects = await getClient().core().projects().list({}, {
+      continue: continueVal,
+      limit: 20
+    })
     this.setState({
-      projects: (await getClient().core().projects().list()).items
+      projects: projects.concat(newProjects.items),
+      continueVal: newProjects.metadata.continue || ""
     })
   }
 
@@ -41,10 +72,16 @@ export default class ProjectList extends React.Component<ProjectListProps, Proje
       return <p>Log in to view projects.</p>
     }
     const projects = this.state.projects
-    const projectListItems = projects.map((project: core.Project) => {
-      return <ProjectListItem project={project}/>
-    })
-    return <ul>{projectListItems}</ul>
+    const hasMore = this.state.continueVal != ""
+    return (
+      <div>
+        <InfiniteScroll dataLength={this.state.projects.length} next={this.fetch} hasMore={hasMore} loader={<h4>Loading...</h4>}>
+          {projects.map((project: core.Project) => (
+            <ProjectListItem key={project.metadata.id} project={project}/>
+          ))}
+        </InfiniteScroll>
+      </div>
+    )
   }
 
 }
