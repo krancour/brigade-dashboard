d32848d9767fd03bf3143480ce65ab6ac1a4371e
Kent Rancourt <kent.rancourt@microsoft.com>
2022-01-12 16:18:48 -0500

    fix routing
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React from "react"
-import { Link, Routes, Route, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 import getClient from "./Client"
 import * as config from "./Config"
-import Event from "./Event"
-import EventList from "./EventList"
 import LoginControl from "./LoginControl"
-import Project from "./Project"
-import ProjectList from "./ProjectList"
-import ServiceAccount from "./ServiceAccount"
-import ServiceAccountList from "./ServiceAccountList"
-import User from "./User"
-import UserList from "./UserList"
 
 import './App.css'
 
@@ -51,40 +43,20 @@ export default class App extends React.Component<AppProps, AppState> {
   render(): React.ReactElement {
     const loggedIn = this.state.loggedIn
     return (
-        <div>
-          <header>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/projects">Projects</Link></li>
-              <li><Link to="/events">Events</Link></li>
-              <li><Link to="/users">Users</Link></li>
-              <li><Link to="/service-accounts">Service Accounts</Link></li>
-            </ul>
-            <LoginControl loggedIn={loggedIn} onLogin={this.handleLogin} onLogout={this.handleLogout}/>
-            {/* TODO: Need to put some kind of breadcrumbs here */}
-          </header>
-          { 
-            loggedIn // If logged in, use routes; else tell the user to log in
-            ? 
-              <div>
-                <Routes>
-                  <Route path='/' element={<ProjectList/>}/>
-                  <Route path='/projects' element={<ProjectList/>}/>
-                  <Route path="/projects/:id" element={<Project/>}/>
-                  <Route path='/events' element={<EventList/>}/>
-                  <Route path='/events/:id' element={<Event/>}/>
-                  <Route path='/users' element={<UserList/>}/>
-                  <Route path='/users/:id' element={<User/>}/>
-                  <Route path='/service-accounts' element={<ServiceAccountList/>}/>
-                  <Route path='/service-accounts/:id' element={<ServiceAccount/>}/>
-                  <Route path="*" element={<h1>404</h1>}/>
-                </Routes>
-                <Outlet/>
-              </div>
-            :
-              <div className="box">Log in to see this content.</div>
-          }
-        </div>
+      <div>
+        <header>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/events">Events</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/service-accounts">Service Accounts</Link></li>
+          </ul>
+          <LoginControl loggedIn={loggedIn} onLogin={this.handleLogin} onLogout={this.handleLogout}/>
+          {/* TODO: Need to put some kind of breadcrumbs here */}
+        </header>
+        { loggedIn ? <Outlet/> : <div className="box">Log in to see this content.</div> }
+      </div>
     )
   }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css'
+
 import App from './App'
+import Event from "./Event"
+import EventList from "./EventList"
+import Project from "./Project"
+import ProjectList from "./ProjectList"
+import ServiceAccount from "./ServiceAccount"
+import ServiceAccountList from "./ServiceAccountList"
+import User from "./User"
+import UserList from "./UserList"
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App/>}>
+          <Route index element={<ProjectList/>}/>
+          <Route path="projects" element={<ProjectList/>}/>
+          <Route path="projects/:id" element={<Project/>}/>
+          <Route path="events" element={<EventList/>}/>
+          <Route path="events/:id" element={<Event/>}/>
+          <Route path="users" element={<UserList/>}/>
+          <Route path="users/:id" element={<User/>}/>
+          <Route path="service-accounts" element={<ServiceAccountList/>}/>
+          <Route path="service-accounts/:id" element={<ServiceAccount/>}/>
+          <Route path="*" element={<h1>404</h1>}/>
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
