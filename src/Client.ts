d49d2abcb0e50c4de752b02335491cdf4c604b5e
Kent Rancourt <kent.rancourt@microsoft.com>
2022-01-14 17:52:37 -0500

    set up nginx as reverse proxy for api
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- src/Client.ts
+++ src/Client.ts
@@ -1,7 +1,7 @@
 import { APIClient } from "@brigadecore/brigade-sdk"
 
 import * as consts from "./Consts"
-import config from "./config.json"
+import * as config from "./Config"
 
 export default function getClient(): APIClient {
   const token = localStorage.getItem(consts.brigadeAPITokenKey) || ""
