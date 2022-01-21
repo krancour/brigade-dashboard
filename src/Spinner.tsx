699a13054fa6fd0b8a87ce4eb8c723d2b410308f
Kent Rancourt <kent.rancourt@microsoft.com>
2022-01-21 13:46:22 -0500

    change color of spinner
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- src/Spinner.tsx
+++ src/Spinner.tsx
@@ -5,7 +5,11 @@ import BootstrapSpinner from "react-bootstrap/Spinner"
 export default class Spinner extends React.Component {
 
   render(): React.ReactElement {
-    return <div className="spinner-wrapper"><BootstrapSpinner animation="border"/></div>
+    return (
+      <div className="spinner-wrapper">
+        <BootstrapSpinner animation="border" variant="primary"/>
+      </div>
+    )
   }
 
 }
