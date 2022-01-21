f6cb732da13d61becdfae9c8b9e691f8f57cd9ac
Kent Rancourt <kent.rancourt@microsoft.com>
2022-01-21 12:58:38 -0500

    improve yaml syntax highlighting
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- src/YAMLViewer.tsx
+++ src/YAMLViewer.tsx
@@ -4,9 +4,7 @@ import React from "react"
 
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter"
 import yamlSyntax from "react-syntax-highlighter/dist/esm/languages/hljs/yaml"
-import docco from "react-syntax-highlighter/dist/esm/styles/hljs/docco"
-
-import Box from "./Box"
+import style from "react-syntax-highlighter/dist/esm/styles/hljs/sunburst"
 
 SyntaxHighlighter.registerLanguage('yaml', yamlSyntax)
 
@@ -18,11 +16,9 @@ export default class YAMLViewer extends React.Component<YAMLViewerProps> {
 
   render(): React.ReactElement {
     return (
-      <Box>
-        <SyntaxHighlighter language="yaml" style={docco}>
-          {yaml.dump(this.props.object)}
-        </SyntaxHighlighter>
-      </Box>
+      <SyntaxHighlighter language="yaml" style={style}>
+        {yaml.dump(this.props.object)}
+      </SyntaxHighlighter>
     )
   }
 
