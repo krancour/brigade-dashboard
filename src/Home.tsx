98b7dfea2f42c968b3ff4ec7d190480d6b576de8
Kent Rancourt <kent.rancourt@microsoft.com>
2022-01-28 12:19:05 -0500

    work on home page
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,10 @@
+import { faGithub, faSlack, faYoutube } from "@fortawesome/free-brands-svg-icons"
+import { faBlog, faBook } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 import React from "react"
+
+import { Link } from "react-router-dom"
 
 import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
@@ -37,18 +43,58 @@ export default class Home extends React.Component {
           <Col>
             <Card bg="light">
               <Card.Header>
-                Placeholder
+                Resources
               </Card.Header>
               <Card.Body>
-                Placeholder
+                <ul className="links">
+                  <li>
+                    <FontAwesomeIcon icon={faBook}/>
+                    &nbsp;&nbsp;
+                    <a href="https://docs.brigade.sh/" target="_blank">Documentation</a>
+                    <ul>
+                      <li>
+                        <a href="https://quickstart.brigade.sh/" target="_blank">QuickStart</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faBlog}/>
+                    &nbsp;&nbsp;
+                    <a href="https://blog.brigade.sh/" target="_blank">Blog</a>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faYoutube}/>
+                    &nbsp;&nbsp;
+                    <a href="https://youtube.brigade.sh/" target="_blank">YouTube Channel</a>
+                    <ul>
+                      <li>
+                        <a href="https://www.youtube.com/watch?v=VFyvYOjm6zc" target="_blank">Video QuickStart</a>
+                      </li>
+                      <li>
+                        <a href="https://www.youtube.com/playlist?list=PLUfRhEZrmeaSWQCvOSs3bbq1NpbSVCW6v" target="_blank">Brigade in 5 Minutes Series</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faSlack}/>
+                    &nbsp;&nbsp;
+                    <a href="https://slack.brigade.sh/" target="_blank">Slack Channel</a>
+                  </li>
+                </ul>
               </Card.Body>
             </Card>
             <Card bg="light">
               <Card.Header>
-                Placeholder
+                Contribute
               </Card.Header>
               <Card.Body>
-                Placeholder
+                <ul className="links">
+                  <li>
+                    <FontAwesomeIcon icon={faGithub}/>
+                    &nbsp;&nbsp;
+                    <a href="https://github.com/brigadecore" target="_blank">Check us out on GitHub!</a>
+                  </li>
+                </ul>
               </Card.Body>
             </Card>
           </Col>
