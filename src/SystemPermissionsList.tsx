1afa89aeabc2c7cb5392d9369280fc4f20959f77
Kent Rancourt <kent.rancourt@microsoft.com>
2022-01-28 14:42:38 -0500

    suppress principal column when showing system-level permissions for a given principal
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- src/SystemPermissionsList.tsx
+++ src/SystemPermissionsList.tsx
@@ -14,6 +14,7 @@ const permissionsListPageSize = 20
 
 interface SystemPermissionsListItemProps {
   roleAssignment: libAuthz.RoleAssignment
+  suppressPrincipalColumn?: boolean
 }
 
 interface SystemPermissionsListItemState {
@@ -30,28 +31,32 @@ class SystemPermissionsListItem extends React.Component<SystemPermissionsListIte
   }
 
   async componentDidMount(): Promise<void> {
-    let locked: boolean | null = null
-    switch (this.props.roleAssignment.principal.type) {
-      case authz.PrincipalTypeServiceAccount:
-        locked = (await getClient().authn().serviceAccounts().get(this.props.roleAssignment.principal.id)).locked? true : false
-        break
-      case authz.PrincipalTypeUser:
-        locked = (await getClient().authn().users().get(this.props.roleAssignment.principal.id)).locked? true : false
-        break
+    if (!this.props.suppressPrincipalColumn) {
+      let locked: boolean | null = null
+      switch (this.props.roleAssignment.principal.type) {
+        case authz.PrincipalTypeServiceAccount:
+          locked = (await getClient().authn().serviceAccounts().get(this.props.roleAssignment.principal.id)).locked? true : false
+          break
+        case authz.PrincipalTypeUser:
+          locked = (await getClient().authn().users().get(this.props.roleAssignment.principal.id)).locked? true : false
+          break
+      }
+      this.setState({
+        locked: locked
+      })
     }
-    this.setState({
-      locked: locked
-    })
   }
 
   render(): React.ReactElement {
     return (
       <tr>
-        <td>
-          <PrincipalIcon principalType={this.props.roleAssignment.principal.type} locked={this.state.locked}/>&nbsp;&nbsp;
-          {/* TODO: Make this link to the user or service account */}
-          {this.props.roleAssignment.principal.id}
-        </td>
+        { this.props.suppressPrincipalColumn ? null : (
+            <td>
+              <PrincipalIcon principalType={this.props.roleAssignment.principal.type} locked={this.state.locked}/>&nbsp;&nbsp;
+              {/* TODO: Make this link to the user or service account */}
+              {this.props.roleAssignment.principal.id}
+            </td>
+        )}
         <td>{this.props.roleAssignment.role}</td>
         <td>{this.props.roleAssignment.scope}</td>
       </tr>
@@ -71,12 +76,13 @@ export default withPagingControl(
       limit: permissionsListPageSize
     })
   },
-  (roleAssignments: libAuthz.RoleAssignment[]): React.ReactElement => {
+  (roleAssignments: libAuthz.RoleAssignment[], props: SystemPermissionsListProps): React.ReactElement => {
+    const suppressPrincipalColumn = props.selector?.principal ? true : false
     return (
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>Principal</th>
+            { suppressPrincipalColumn ? null : <th>Principal</th> }
             <th>Role</th>
             <th>Scope</th>
           </tr>
@@ -87,6 +93,7 @@ export default withPagingControl(
               <SystemPermissionsListItem 
                 key={roleAssignment.principal.type + ":" + roleAssignment.principal.id + ":" + roleAssignment.role}
                 roleAssignment={roleAssignment}
+                suppressPrincipalColumn={suppressPrincipalColumn}
               />
             ))
           }
